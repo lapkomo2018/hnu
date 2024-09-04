1fca6684c940c10a790b72636ba96e845b9f0f54
lapkomo2018 <lapkomo2018@gmail.com>
2024-09-04 14:55:00 +0300

    init

---
+++ oop/1/code/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task1;
+class Task1 {
+    const double g = 9.807;
+    static double E(double m, double v) => m*Math.Pow(v, 2)/2;
+    static double P(double m, double h) => m*g*h; 
+    
+    static void Main() {
+        Console.WriteLine("Input m: ");
+        double m = double.Parse(Console.ReadLine());
+    
+        Console.WriteLine("Input h: ");
+        double h = double.Parse(Console.ReadLine());
+    
+        Console.WriteLine("Input v: ");
+        double v = double.Parse(Console.ReadLine());
+    
+        Console.WriteLine($"m: {m}; h: {h}; v: {v}");
+        Console.WriteLine($"E: {E(m,v)}\nP: {P(m,h)}");
+    }
+}
---
+++ oop/1/code/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2;
+
+using static System.Math;
+
+class Task2
+{
+    const double V(double x, double b, double a, int i) => Math.Tan(x/i + a) - Math.Log10(Math.Abs(b*i+7));
+    
+        
+    static void Main()
+    {
+        Console.WriteLine("Input x: ");
+        double x = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Input a: ");
+        double a = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Input b: ");
+        double b = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Input i: ");
+        int i = int.Parse(Console.ReadLine());
+        
+    }
+}
